4fbdf45d68654191d967e1740ad6fce285118d09
Fluxhydra <fluxhydra@gmail.com>
2019-10-30 19:50:21 +0700

    Tinggal cube nya

--- index.js
+++ index.js
@@ -44,39 +44,163 @@
       // 0.12375, 0.0, 0.0, 0.3 //DB
 
       //Segitiga
-      // 0.0, 0.7, 0.0, 0.3, -0.5, -0.5, //ABG
-      // 0.0, 0.3, -0.5, -0.5, -0.333, -0.5, //BGH
-      // 0.0, 0.7, 0.0, 0.3, 0.5, -0.5, //ABJ
-      // 0.0, 0.3, 0.333, -0.5, 0.5, -0.5, //BIJ
-      // -0.12375, 0.0, -0.193875, -0.17, 0.193875, -0.17, //CEF
-      // -0.12375, 0.0, 0.12375, 0.0, 0.193875, -0.17 //CDF
+      0.0, 0.7, 0.0, 0.3, -0.5, -0.5, //ABG
+      0.0, 0.3, -0.5, -0.5, -0.333, -0.5, //BGH
+      0.0, 0.7, 0.0, 0.3, 0.5, -0.5, //ABJ
+      0.0, 0.3, 0.333, -0.5, 0.5, -0.5, //BIJ
+      -0.12375, 0.0, -0.193875, -0.17, 0.193875, -0.17, //CEF
+      -0.12375, 0.0, 0.12375, 0.0, 0.193875, -0.17 //CDF
 
-      //Segitiga A
-      0.5, 0.7, 0.5, 0.3, 0.0, -0.5, //ABG
-      0.5, 0.3, 0.0, -0.5, 0.167, -0.5, //BGH
-      0.5, 0.7, 0.5, 0.3, 1.0, -0.5, //ABJ
-      0.5, 0.3, 0.833, -0.5, 1.0, -0.5, //BIJ
-      0.37625, 0.0, 0.306125, -0.17, 0.693875, -0.17, //CEF
-      0.37625, 0.0, 0.62375, 0.0, 0.693875, -0.17 //CDF
+      // //Segitiga A
+      // 0.5, 0.7, 0.5, 0.3, 0.0, -0.5, //ABG
+      // 0.5, 0.3, 0.0, -0.5, 0.167, -0.5, //BGH
+      // 0.5, 0.7, 0.5, 0.3, 1.0, -0.5, //ABJ
+      // 0.5, 0.3, 0.833, -0.5, 1.0, -0.5, //BIJ
+      // 0.37625, 0.0, 0.306125, -0.17, 0.693875, -0.17, //CEF
+      // 0.37625, 0.0, 0.62375, 0.0, 0.693875, -0.17 //CDF
     ];
 
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     
-    var aPosition = gl.getAttribLocation(program, 'aPosition');
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition);
-
-    var thetaLocation = gl.getUniformLocation(program, 'theta');
-    var theta = 0.0;
+    var vPosition = gl.getAttribLocation(program, 'vPosition');
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
 
     var oColor = gl.getUniformLocation(program, 'oColor');
     var opacity = 1.0;
     var colorFlag = 1;
 
+    // keyboard input
+    function onKeyDown(event)
+    {
+      switch(event.keyCode)
+      {
+        // Change camera: eye
+        case 81: //q
+          eyeX += 0.5;
+          break;
+        case 87: //w
+          eyeX -= 0.5;
+          break;
+        case 69: //e
+          eyeY += 0.5;
+          break;
+        case 82: //r
+          eyeY -= 0.5;
+          break;
+        case 84: //t
+          eyeZ += 0.5;
+          break;
+        case 89: //y
+          eyeZ -= 0.5;
+          break;
+      }
+    }
+    document.addEventListener('keydown', onKeyDown);
+
+    // model matrix
+    var mmLoc = gl.getUniformLocation(program, 'modelMatrix');
+    var theta = 0.0;
+
+    // Definisi matriks pandangan (view matrix)
+    var vmLoc = gl.getUniformLocation(program, 'viewMatrix');
+    var vm = glMatrix.mat4.create();
+    var eyeX = 0.0, eyeY = 0.0, eyeZ = 10.0, atX = 0.0, atY = 0.0, atZ = 0.0, upX = 0.0, upY = 1.0, upZ = 1.0;
+
+    // Definisi matriks proyeksi perspektif 
+    var pmLoc = gl.getUniformLocation(program, 'perspectiveMatrix');
+    var pm = glMatrix.mat4.create();
+    glMatrix.mat4.perspective(pm,
+      glMatrix.glMatrix.toRadian(100), // fovy dalam radian
+      canvas.width / canvas.height,
+      0.1,  // near
+      25.0  // far
+    );
+    gl.uniformMatrix4fv(pmLoc, false, pm);
+
+    var current = [0.0, 0.0, 0.0];
+    var myFlag = [0, 0, 0];
+    var min = -30, max = 30;
+    for(var a = 0; a < 3; a++)
+    {
+      current[a] = (Math.random() * (+max - +min) + +min)/10;
+    }
+
     function render()
     {
+      var mm = glMatrix.mat4.create();
+      theta += 0.1087;
+      glMatrix.mat4.translate(mm, mm, current);
+
+      // Logika mantul dengan cube 10 x 10 dan range -5 sd 5
+      if(current[0]>5)
+      {
+        myFlag[0] = 1;
+      }
+      else if(current[0]<-5)
+      {
+        myFlag[0] = 0;
+      }
+
+      if(current[1]>5)
+      {
+        myFlag[1] = 1;
+      }
+      else if(current[1]<-5)
+      {
+        myFlag[1] = 0;
+      }
+
+      if(current[2]>5)
+      {
+        myFlag[2] = 1;
+      }
+      else if(current[2]<-5)
+      {
+        myFlag[2] = 0;
+      }
+
+      // Translasi bebas
+      if(myFlag[0]==0)
+      {
+        current[0] += 0.1;
+      }
+      else if(myFlag[0]==1)
+      {
+        current[0] -= 0.1;
+      }
+
+      if(myFlag[1]==0)
+      {
+        current[1] += 0.1;
+      }
+      else if(myFlag[1]==1)
+      {
+        current[1] -= 0.1;
+      }
+
+      if(myFlag[2]==0)
+      {
+        current[2] += 0.1;
+      }
+      else if(myFlag[2]==1)
+      {
+        current[2] -= 0.1;
+      }
+
+      glMatrix.mat4.rotateY(mm, mm, theta);
+      gl.uniformMatrix4fv(mmLoc, false, mm);
+
+      // View matrix
+      glMatrix.mat4.lookAt(vm,
+        glMatrix.mat3.fromValues(eyeX, eyeY, eyeZ), // eye: posisi kamera
+        glMatrix.mat3.fromValues(atX, atY, atZ), // at: posisi kamera menghadap
+        glMatrix.mat3.fromValues(upX, upY, upZ)  // up: posisi arah atas kamera
+      );
+      gl.uniformMatrix4fv(vmLoc, false, vm);
+
       // Bersihkan layar jadi hitam
       gl.clearColor(0.0, 0.0, 0.0, 1.0);
     
@@ -90,10 +214,6 @@
       if(colorFlag == 1) opacity -= 0.005;
       else opacity += 0.005;
       gl.uniform1f(oColor, opacity);
-
-      // Kecepatan rotasi Y
-      theta += 0.01;
-      gl.uniform1f(thetaLocation, theta);
 
       //Gambar A
       gl.drawArrays(gl.TRIANGLES, 0, 18);
--- vertex.glsl
+++ vertex.glsl
@@ -1,27 +1,12 @@
 precision mediump float;
 
-attribute vec4 aPosition;
-uniform float theta;
+attribute vec4 vPosition;
 uniform mat4 viewMatrix;
 uniform mat4 perspectiveMatrix;
+uniform mat4 modelMatrix;
 
 void main()
 {
-  mat4 rotateY = mat4
-  (
-    cos(theta), 0.0, -sin(theta), 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    sin(theta), 0.0 ,cos(theta), 0.0,
-    0.0, 0.0, 0.0, 1.0
-  );
-
-  mat4 translateLeft5Pts = mat4(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    -0.5, 0.0, 0.0 , 1.0
-  );
-
   //Rotasi Y terhadap huruf A
-  gl_Position = rotateY * translateLeft5Pts * aPosition;
+  gl_Position = perspectiveMatrix * viewMatrix * modelMatrix * vPosition;
 }
